9dbfb0fac399f262b6245a16f2e97ecd46edd74b
abasedape <Skylor808@gmail.com>
2024-08-30 12:36:20 -1000

    initial commit

---
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -0,0 +1,6 @@
+export const menuIconImports = `import { InboxArrowDownIcon } from "@heroicons/react/24/outline";`;
+export const menuObjects = `{
+    label: "Public Feedback",
+    href: "/publicfeedback",
+    icon: <InboxArrowDownIcon className="h-4 w-4" />,
+}`;
